457fe5c252a22ea98002f55702c193ee9364e899
sridhar <sridhar.meesala2000@gmail.com>
2024-05-26 23:34:12 +0530

    password generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="generator">
+          <h2 className="genheader">
+            Password Generator
+          </h2>
+          <div className="genpassword">
+            <h3>Passwod</h3>
+            <button className="copybtn"></button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
